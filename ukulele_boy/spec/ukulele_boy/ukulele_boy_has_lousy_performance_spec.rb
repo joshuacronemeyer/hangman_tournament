b189eac411a197d0263cdf8045e1d82790b120cd
Josh Cronemeyer <giosue_c@hotmail.com>
2009-09-13 10:23:55 -0500

    use real wordlist so i can see real performance

--- ukulele_boy/spec/ukulele_boy/ukulele_boy_has_lousy_performance_spec.rb
+++ ukulele_boy/spec/ukulele_boy/ukulele_boy_has_lousy_performance_spec.rb
@@ -1,20 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 require 'ukulele_boy/ukulele_boy'
+require File.expand_path(File.dirname(__FILE__) + '/words')
 
 describe UkuleleBoy::UkuleleBoy do
   
-  WORDS = ["SHAKESPEARE", "WAS", "BORN", "RAISED", "MARRIED", "ANNE", "HATHAWAY", "THREE", "CHILDREN", "SUSANNA", "TWINS", "HAMNET", "JUDITH", "BETWEEN", "BEGAN", "SUCCESSFUL", "CAREER", "LONDON", "WRITER", "PART", "OWNER", "PLAYING", "COMPANY", "CALLED", "LORD", "CHAMBERLAIN", "LATER", "KNOWN", "THE", "KING", "HAVE", "RETIRED", "STRATFORD", "AROUND", "PRIVATE", "CONSIDERABLE", "SPECULATION", "ABOUT", "SUCH", "MATTERS", "APPEARANCE", "SEXUALITY", "RELIGIOUS", "BELIEFS", "PRODUCED", "EARLY", "PLAYS", "WERE", "MAINLY", "COMEDIES", "HISTORIES", "GENRES", "RAISED", "SOPHISTICATION", "ARTISTRY", "SIXTEENTH", "CENTURY", "TRAGEDIES", "INCLUDING", "HAMLET", "KING", "LEAR", "MACBETH", "CONSIDERED", "TRAGICOMEDIES", "KNOWN", "ROMANCES", "COLLABORATED", "WITH", "OTHER", "PLAYWRIGHTS", "RESPECTED", "PLAYWRIGHT", "NINETEENTH", "ROMANTICS", "PARTICULAR", "ACCLAIMED", "GENIUS", "VICTORIANS", "WORSHIPPED", "REVERENCE", "GEORGE", "BERNARD", "SHAW", "CALLED,TWENTIETH", "CENTURY", "REPEATEDLY", "ADOPTED", "REDISCOVERED", "MOVEMENTS", "SCHOLARSHIP", "PERFORMANCE", "CONSTANTLY", "STUDIED", "PERFORMED", "REINTERPRETED", "DIVERSE", "CULTURAL", "POLITICAL", "CONTEXTS", "THROUGHOUT"]
- 
- WORDS_WORDS_WORDS = WORDS + WORDS + WORDS + WORDS + WORDS + WORDS 
-
   it "should not be slow in gameplay lifecycle" do
     start = Time.now
     player = UkuleleBoy::UkuleleBoy.new
-    player.word_list = WORDS_WORDS_WORDS
+    player.word_list = WORDS
     player.new_game(6)
     guess = player.guess("________S", 6)
     player.incorrect_guess(guess)
     elapsed_time = Time.now - start
-    elapsed_time.should < 0.002
+    elapsed_time.should < 0.02
   end
 end
