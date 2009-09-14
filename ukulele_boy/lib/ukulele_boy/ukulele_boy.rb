5628182b28c6e221b0a325e1aece04a87dc7fc6d
Josh Cronemeyer <giosue_c@hotmail.com>
2009-09-13 23:38:51 -0500

    improve naming and trim a couple more unnecessary lines

--- ukulele_boy/lib/ukulele_boy/ukulele_boy.rb
+++ ukulele_boy/lib/ukulele_boy/ukulele_boy.rb
@@ -1,33 +1,21 @@
 require 'set'
-
 module UkuleleBoy
   class UkuleleBoy
     attr_reader :word_list
-    # You may initialize you player but the the initialize method must take NO paramters.
-    # The player will only be instantiated once, and will play many games.
+    
     def initialize
-      @previous_guesses = []
-      @pruned_for_size = false
     end
 
-    # Before starting a game, this method will be called to inform the player of all the possible words that may be
-    # played.
     def word_list=(list)
       @original_word_list = Array.new(list)
     end
 
-    # a new game has started.  The number of guesses the player has left is passed in (default 6),
-    # in case you want to keep track of it.
     def new_game(guesses_left)
       @word_list = Array.new(@original_word_list)
       @previous_guesses = []
       @pruned_for_size = false
     end
 
-    # Each turn your player must make a guess.  The word will be a bunch of underscores, one for each letter in the word.
-    # after your first turn, correct guesses will appear in the word parameter.  If the word was "shoes" and you guessed "s",
-    # the word parameter would be "s___s", if you then guess 'o', the next turn it would be "s_o_s", and so on.
-    # guesses_left is how many guesses you have left before your player is hung.
     def guess(word, guesses_left)
       prune_word_list(word)
       guess = letters.shift
@@ -35,22 +23,17 @@ module UkuleleBoy
       return guess
     end
 
-    # notifies you that your last guess was incorrect, and passes your guess back to the method
     def incorrect_guess(guess)
       @word_list.delete_if{ |word| word.include? guess }
     end
 
-    # notifies you that your last guess was correct, and passes your guess back to the method
     def correct_guess(guess)
       @word_list.delete_if{ |word| !word.include? guess }
     end
 
-    # you lost the game.  The reason is in the reason parameter
     def fail(reason)
     end
 
-    # The result of the game, it'll be one of 'win', 'loss', or 'fail'.
-    # The spelled out word will be provided regardless of the result.
     def game_result(result, word)
     end
 
@@ -66,7 +49,6 @@ module UkuleleBoy
       return remaining_letters
     end
 
-    #private
     def order_by_frequency(letters)
       most_frequent_letters = ['e', 't', 'a', 'o', 'i', 'n', 's', 'r', 'h', 'l', 'd', 'c']
       most_frequent_letters.delete_if { |letter| !letters.include?(letter) }
@@ -82,21 +64,20 @@ module UkuleleBoy
       @word_list.delete_if{ |word| word.size != size }
     end
 
-    def prune_for_position(word)
+    def prune_for_structure(word)
       word.scan(/./).each_with_index do |letter, index|
         next if letter == "_"
-        exclude_for_wrong_letter(letter, index)
+        exclude_words_with_wrong_letter_structure(letter, index)
       end
     end
 
     def prune_word_list(word)
       prune_for_size(word.size) unless @pruned_for_size
-      prune_for_position(word)
+      prune_for_structure(word)
     end
 
-    def exclude_for_wrong_letter(letter, index)
+    def exclude_words_with_wrong_letter_structure(letter, index)
       @word_list.delete_if{ |word| letter != word[index, 1]}
     end
-
   end
 end
